4d3a23cefc19ced96e885cdb99ac93a2db15ce80
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-04 14:28:52 -0400

    Used geolocation api to retrieve location.

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,30 @@
 //Christine - Establish URL source and API key.
 var APIKey = "2015c5f25f689dcf6dedba026011e032";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&units=metric&appid=" + APIKey;
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&units=metric&appid=" + APIKey;
 
-//Christine - Establish an AJAX call to the query source.
-
-//------------------------------------------------------------------------------------------
-function acquireData() {
-    fetch(queryURL, {
+//Christine - Use the fetch api function to retrieve data from the openweathermap.org site.
+function fetchData(URL) {
+    fetch(URL, {
         method: "GET",
 }) 
-        .then(response => response.json())
-        .then(data => fetchData(data))
+    .then(response => response.json())
+    .then(data => displayData(data))
 }
-acquireData();
+fetchData(weatherURL);
 
 //Christine - Add current location, weather, time, and date information to NavBar.
-function fetchData(data) {
+function displayData(data) {
     console.log(data);
-    document.querySelector(".city").innerHTML = `${data.name} <br> ${data.main.temp} <br> ${moment().format('MMMM Do YYYY, h:mm a')}`;
+    document.querySelector(".navbarInfo").innerHTML = `${data.name} <br> ${data.main.temp} <br> ${moment().format('MMMM Do YYYY, h:mm a')}`;
 }
 
-//Test Comment
+// Christine - Use geolocation api to retrieve location.
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position){
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var coordURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey}`
+        fetchData(coordURL)
+    })
+}
+getLocation();
